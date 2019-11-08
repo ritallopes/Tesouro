2397aceb7de24025798e67cf9d54071ca5f13a84
Rita Lopes <ritalino99@gmail.com>
2019-11-08 18:12:16 -0300

    apagando modificações em banco

--- src/br/com/ufrn/imd/lpii/classes/main/Main.java
+++ src/br/com/ufrn/imd/lpii/classes/main/Main.java
@@ -10,38 +10,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        ConnectionCategoria connectionCategoria = new ConnectionCategoria();
-        connectionCategoria.conectar();
-        connectionCategoria.criarTabela();
-
-        connectionCategoria.cadastrarCategoria( "categoria1", "teste de categoria");
-        connectionCategoria.cadastrarCategoria("categoria2", "teste de categoria");
-        connectionCategoria.cadastrarCategoria( "categoria2", "teste de categoria");
-
-        ConnectionLocalizacao connectionLocalizacao = new ConnectionLocalizacao();
-        connectionLocalizacao.conectar();
-        connectionLocalizacao.criarTabela();
-
-        connectionLocalizacao.cadastrarLocalizacao( "Localizacao1", "teste de Localizacao");
-
-
-        ConnectionBem connectionBem = new ConnectionBem();
-        connectionBem.conectar();
-        connectionBem.criarTabela();
-
-        connectionBem.cadastrarBem( "Localizacao1", "teste de Localizacao", 1, 1);
-
-
-
-
-
-
-         connectionBem.desconectar();
-        connectionLocalizacao.desconectar();
-
-        connectionCategoria.desconectar();
-
-
+       
        Bot.inicializacaoBot("1048746356:AAEDDgr7PPTnQ0hQuxSaZdDp3AVVYErsTDc");
 
 
